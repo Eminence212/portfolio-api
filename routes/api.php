fb0b7ed241ea0c23efd1cd76ba8b822c103cb0ba
Eminence <mulemanowa@gmail.com>
2021-11-10 13:12:07 +0100

    skills types routes added

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\IdentitieController;
 use App\Http\Controllers\OpinionController;
+use App\Http\Controllers\TypeController;
 
 
 // ---------------------Identities----------------------------
@@ -28,3 +29,11 @@ Route::get('opinion/{id}',[OpinionController::class,'show']);
 Route::post('opinion',[OpinionController::class,'store']);
 Route::put('opinion/{id}',[OpinionController::class,'update']);
 Route::delete('opinion/{id}',[OpinionController::class,'destroy']);
+
+// ---------------------Skills Types----------------------------
+
+Route::get('types',[TypeController::class,'index']);
+Route::get('type/{id}',[TypeController::class,'show']);
+Route::post('type',[TypeController::class,'store']);
+Route::put('type/{id}',[TypeController::class,'update']);
+Route::delete('type/{id}',[TypeController::class,'destroy']);
