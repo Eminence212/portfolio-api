9de87a5c8293dea6244eb232cdced6d4d917d874
Eminence <mulemanowa@gmail.com>
2021-11-10 10:02:07 +0100

    store controller added

--- app/Http/Controllers/IdentitieController.php
+++ app/Http/Controllers/IdentitieController.php
@@ -1,10 +1,79 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Identitie;
+use App\Http\Resources\IdentitieResource;
+
 
 class IdentitieController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         //Create a new identities
+        $identitie = new Identitie();
+        $identitie->firstName = $request->input('firstName');
+        $identitie->lastName = $request->input('lastName');
+        $identitie->phoneNumber = $request->input('phoneNumber');
+        $identitie->adresse = $request->input('adresse');
+        $identitie->twitter = $request->input('twitter');
+        $identitie->linkedin = $request->input('linkedin');
+        $identitie->facebook = $request->input('facebook');
+        $identitie->instagram = $request->input('instagram');
+        $identitie->skype = $request->input('skype');
+        $identitie->save();
+        return new IdentitieResource($identitie);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- app/Identitie.php
+++ app/Identitie.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Identitie extends Model
 {
     //
+     protected $fillable = [
+        'firstName',
+        'lastName',
+        'phoneNumber',
+        'adresse',
+        'twitter',
+        'linkedin',
+        'facebook',
+        'instagram',
+        'skype'
+    ];
 }
